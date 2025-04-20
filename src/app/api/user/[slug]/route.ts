17de83426c5b68ea30da2ed6a415f5a81d9f9918
Evgenii Kondrashkov <kondrashkov@mail.ru>
2025-04-20 19:05:14 +0200

    minor

--- src/app/api/user/[slug]/route.ts
+++ src/app/api/user/[slug]/route.ts
@@ -1,6 +1,8 @@
 import prisma from "@/lib/prisma"
 import { User, UserAvatar, UserCover } from "@/types/types"
 
+export const dynamic = "force-dynamic"
+
 export async function GET(
   request: Request,
   { params }: { params: Promise<{ slug: string }> }
--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,6 +1,8 @@
 import prisma from "@/lib/prisma"
 import { User } from "@/types/types"
 
+export const dynamic = "force-dynamic"
+
 export async function GET() {
   try {
     const users =
